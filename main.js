1f8db18a7143383d83360c34b45dbddc8bb988df
Sam Respass <samrespass@gmail.com>
2022-06-07 15:42:03 -0400

    hittin the showers

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let guitar = document.querySelector("#salsa")
         let maraca = flamenco.value
         event.preventDefault();
         console.log(maraca)
-        let url= 'https://itunes.apple.com/search?term=' + maraca
+        let url= 'https://itunes.apple.com/search?term=' + maraca + "&media=music" + "&limit=10"
         fetching(url)
         })
 
@@ -18,16 +18,40 @@ fetch(url, {
     return response.json()}
     ) 
 .then(function (blues){
-   buildProfile(blues)
+    console.log(blues.results)
+   buildProfile(blues.results)
 })
 }
 
 function buildProfile (jazzy) {
+    if (nigo.innerText != null) {
+        killEm()
+    }
     console.log(jazzy)
     let hipHop = document.createElement('div')
     hipHop.classList.add("searchResults")
     nigo.appendChild(hipHop)
-} 
+    for (let jazz of (jazzy)) {
+        let saxElement = document.createElement('img')
+        saxElement.classList.add("albumArt")
+        saxElement.src = jazz.artworkUrl100
+        saxElement.addEventListener("click", function () {
+            let celloElement = document.createElement("audio controls")
+            celloElement.src = jazz.previewUrl
+            hipHop.appendChild(nigo)
+        }
+        )
+        hipHop.appendChild(saxElement)
+        let jazzyElement = document.createElement('div')
+        jazzyElement.classList.add('result')
+        jazzyElement.innerText = jazz.artistName +' ' + jazz.trackName
+        hipHop.appendChild(jazzyElement)
+    }
+}
+
+function killEm () {
+    nigo.innerText = null;
+}
 // Making our HTML divs into values here
 let ichigo = document.querySelector('#ichi')
 let nigo = document.querySelector("#ni")
